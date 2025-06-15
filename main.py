0442886b4ef2e3b4b1a9265ecfc4d73600ed6637
OniEikichi03 <baccarinimattiawork@gmail.com>
2025-06-15 18:08:43 +0200

    added count_words and started count_characters in stats.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from stats import count_words
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+
 def main():
     contents = get_book_text("books/frankenstein.txt")
-    print(contents)
+    print(f"{count_words(contents)} words found in the document")
+
 
 main()
---
+++ stats.py
@@ -0,0 +1,6 @@
+def count_words(contents):
+    words = contents.split()
+    return len(words)
+
+def count_character(contents):
+    
